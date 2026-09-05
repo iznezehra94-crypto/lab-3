59caa9982ead71763db911f8e563e94a713fd455
iznezehra94-crypto <iznezehra94@gmail.com>
2026-09-05 20:03:45 +0500

    Add basic user input program in Q2.c

---
+++ Q2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int age,student_id;
+char name[20],department[20],university_name[20];
+printf("Enter your name:\n");
+scanf("%s",name);
+printf("Enter your department:\n");
+scanf("%s",department);
+printf("Enter your university name:\n");
+scanf("%s",university_name);
+printf("Enter your age\n");
+scanf("%d",&age);
+printf("Enter your student id:\n");
+scanf("%d",&student_id);
+}
