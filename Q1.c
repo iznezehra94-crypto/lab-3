470143da27262d9d5507aafe4219d10b14ebe3f7
iznezehra94-crypto <iznezehra94@gmail.com>
2026-09-05 19:46:18 +0500

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+printf("My name is Izn-e-Zehra\n");
+printf("My age is 18\n");
+printf("My department is Computer Science\n");
+printf("My student id is 1234\n");
+printf("My section is D\n");
+printf("My university name is FAST\n");
+}
